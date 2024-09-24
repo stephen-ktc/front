2e1c56217af6212e556d74af865d303a1420314f
82255369 <stephen.k@kt.com>
2024-09-24 13:32:00 +0900

    init

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,42 @@
+const { resolve } = require('node:path')
+
+const project = resolve(process.cwd(), 'tsconfig.json')
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@next/next/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+      json: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-base-to-string': 'error',
+    '@typescript-eslint/no-confusing-void-expression': 'error',
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+    '@typescript-eslint/restrict-plus-operands': 'error',
+    '@typescript-eslint/init-declarations': 'error',
+    '@typescript-eslint/dot-notation': 'warn',
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-use-before-define': 'error',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+}
---
+++ packages/ui/.storybook/manager.js
@@ -0,0 +1,19 @@
+/**
+ * storybook manager
+ * @description storybook addons 설정 관리
+ * @see https://storybook.js.org/docs/configure/user-interface/theming
+ * @author stephen.k@kt.com
+ * @date 2024.08.29
+ */
+import { addons } from '@storybook/manager-api';
+import { themes } from '@storybook/theming';
+import logo from '../public/logo/logo-bi-cloud-basic.png'
+
+addons.setConfig({
+    theme: {
+        ...themes.normal,
+        brandTitle: 'kt cloud',
+        brandUrl: 'https://www.ktcloud.com/',
+        brandImage: logo,
+    },
+});
