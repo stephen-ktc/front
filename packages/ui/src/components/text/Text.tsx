05f80fec2ebffb74c2650b1ae5d29c8d4ad53522
82244872 <j.song@kt.com>
2024-10-02 18:03:22 +0900

    ui 패키지 최적화 - react+vite 기반 변경 + 빌드 최적화

--- packages/ui/src/components/text/Text.tsx
+++ packages/ui/src/components/text/Text.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 import './text.css'
 import { TextProps } from './Text.types'
 
-const Text: React.FC<TextProps> = ({ variant = 'p', text }) => {
+export const Text: React.FC<TextProps> = ({ variant = 'p', text }) => {
   const Tag = variant // 'h1', 'p', 'span' 등으로 설정
 
   return <Tag>{text}</Tag>
 }
-
-export default Text
